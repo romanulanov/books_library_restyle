8f723d59d42edf57952ec772551dd04e3a17990c
romanulanov <turnsourcream@gmail.com>
2023-07-10 13:31:51 +0400

    download books from tululu.org

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ if not os.path.exists(directory):
 
 for i in range(1,11):
     url = f"https://tululu.org/txt.php?id={i}"
-
     response = requests.get(url)
     response.raise_for_status() 
-
     filename = f'{directory}id{i}.txt'
     with open(filename, 'wb') as file:
         file.write(response.content)
