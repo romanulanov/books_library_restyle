5f67a2b3e1660350828f7c9821dd19284c45a91a
romanulanov <turnsourcream@gmail.com>
2023-07-10 18:14:07 +0400

    download books with titles

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import validate_filename, sanitize_filename
 
-directory = "books/"
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def check_for_redirect(response):
+    if response.url == "https://tululu.org/":
+        raise requests.exceptions.HTTPError
+
+
+def download_txt(url, filename, folder='books/'):
+    filename = sanitize_filename(filename)
+    response = requests.get(url)
+    try:
+        check_for_redirect(response) 
+    except requests.exceptions.HTTPError as e:  
+        return None
+    response.raise_for_status() 
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    str =  os.path.join(folder,f'{filename}')
+    with open(str, 'wb') as file:
+        file.write(response.content)
+    return str
+    
+url = 'http://tululu.org/txt.php?id=1'
+
+
 
 for i in range(1,11):
-    url = f"https://tululu.org/txt.php?id={i}"
+    #url = f"https://tululu.org/txt.php?id={i}"
+    url = f"https://tululu.org/b{i}"
     response = requests.get(url)
-    response.raise_for_status() 
-    filename = f'{directory}id{i}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    
+    response.raise_for_status()  
+    
+    soup = BeautifulSoup(response.content, 'lxml')
+    title = soup.find('title')
+    download_txt(f"https://tululu.org/txt.php?id={i}", f'{i}. {title.text.partition(" - ")[0].strip()}.txt', folder='books/')
+    #print(f"Заголовок:{title.text.partition(' - ')[0].strip()}\nАвтор:{title.text.partition(' - ')[2].split(',')[0].strip()}")
+      
+   
+          
+    
+
+
+    
+    
