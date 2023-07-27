9ecb26ceca48ffc1236e843b4a8105b45401149a
romanulanov <turnsourcream@gmail.com>
2023-07-27 20:24:24 +0400

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import ast
+from livereload import Server, shell
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -19,5 +20,9 @@ rendered_page = template.render(books=books)
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+server = Server()
+
+server.watch('template.html', shell('make html', cwd='docs'))
+
+server.serve(root='.')
