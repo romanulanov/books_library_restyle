4f718cb2aaf3d358bbfc30f996ab73df74a210b4
romanulanov <turnsourcream@gmail.com>
2023-08-04 18:46:32 +0400

    Починил livereload

--- render_website.py
+++ render_website.py
@@ -26,24 +26,34 @@ def main():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    template = env.get_template('template.html')
+    
 
-    with open(args.dest_json, encoding='utf-8') as json_file:
-        books = json.load(json_file)
-    num_pages = math.ceil(len(books)/NUM_BOOKS_PER_PAGE)
-    books = list(chunked(books, NUM_BOOKS_PER_PAGE))
-    os.makedirs('pages/', exist_ok=True)
+    
 
-    for index, books in enumerate(books, 1):
-        rendered_page = template.render(books=list(chunked(books, NUM_ROWS)),
+    
+
+    def rebuild():
+        template = env.get_template('template.html')
+        with open(args.dest_json, encoding='utf-8') as json_file:
+            books = json.load(json_file)
+        num_pages = math.ceil(len(books)/NUM_BOOKS_PER_PAGE)
+    
+        os.makedirs('pages/', exist_ok=True)
+        books = list(chunked(books, NUM_BOOKS_PER_PAGE))
+        for index, books in enumerate(books, 1):
+            print(index)
+            rendered_page = template.render(books=list(chunked(books, NUM_ROWS)),
                                         num_pages=range(1, num_pages+1),
                                         cur_page=index,
                                         )
-        with open(f'pages/index{index}.html', 'w', encoding='utf8') as file:
-            file.write(rendered_page)
+            with open(f'pages/index{index}.html', 'w', encoding='utf8') as file:
+                file.write(rendered_page)
+        
+
+    rebuild()
 
     server = Server()
-    server.watch('template.html', shell('make html', cwd='docs'))
+    server.watch('template.html', rebuild )
     server.serve(root='.')
 
 
